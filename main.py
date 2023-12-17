cbb9630b30a50c52ec3b0574fa8a1dfabf5f34bc
azureonline <venu466@yahoo.com>
2023-12-16 22:03:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/")
-def home():
-    return "Hello, this is a sample Python Web App running on Flask Framework -- main!"
