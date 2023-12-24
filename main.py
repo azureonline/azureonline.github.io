448999dac9db90974f5316e7871c7a5828e417ad
azureonline <venu466@yahoo.com>
2023-12-23 19:04:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Import Required Modules
 from flask import Flask, render_template
 import pandas as pd
 import matplotlib.pyplot as plt 
@@ -16,7 +17,8 @@ fast_MA = 5
 slow_MA = 9
 initial_wealth = '1000'
 #STK = input("Enter share name : ")
-stock = 'SPY'
+stocksymbols = ['SPY','QQQ','IWM','DIA','SMH']
+
 
 from datetime import date, timedelta
 today = date.today()
@@ -24,12 +26,11 @@ end_date = today.strftime("%Y-%m-%d")
 prevdayrange = today- timedelta(days=6)
 start_date = prevdayrange.strftime("%Y-%m-%d")
 
-@app.route('/')
-def bar_with_plotly():
+def bar_with_plotly(stock):
 
     yf.pdr_override()
     #dwl_df = yf.download(tickers=stock, start=startdate, end=enddate, interval='1d',period='60d')
-    dwl_df = yf.download(tickers=stock, period='1d', interval='15m')
+    dwl_df = yf.download(tickers=stock, period='1d', interval='5m')
     dwl_df.reset_index(inplace=True) 
     dwl_df[['Open','High','Low','Close','Adj Close']] = dwl_df[['Open','High','Low','Close','Adj Close']].round(2)
 
@@ -40,6 +41,22 @@ def bar_with_plotly():
     dwl_df['crossover'] = dwl_df['intersectionpoint'].diff()
     filtered_col_df = dwl_df[['Datetime','Open','High','Low','Close','crossover']].copy()
     rslt_df = filtered_col_df[filtered_col_df['crossover'] != 0.0] 
+    rslt_df['Datetime'] = pd.to_datetime(rslt_df['Datetime'])
+    rslt_df['Datetime'] = rslt_df['Datetime'].dt.strftime('%H:%M')
+    rslt_df.insert(0,'Symbol','')
+    rslt_df['Symbol'] = stock
     
     # Use render_template to pass graphJSON to html
-    return rslt_df.to_html()
+    return rslt_df.tail(4)
+
+
+@app.route('/') 
+def bar_with_plotly1():
+    length = len(stocksymbols)
+    appended_data = pd.DataFrame()
+    for x in stocksymbols:
+        data = bar_with_plotly(x)
+        appended_data = appended_data._append(data,ignore_index=True)
+    appended_data = appended_data.sort_values(by='Datetime',ascending=[False])
+    return """<meta http-equiv="refresh" content="300" />  {}.""".format(appended_data.to_html(index=False))
+    
