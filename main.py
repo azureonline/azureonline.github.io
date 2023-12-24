09e8101b293872da5ff5fb2f50fef309ea220b2f
azureonline <venu466@yahoo.com>
2023-12-23 23:43:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,10 @@ end_date = today.strftime("%Y-%m-%d")
 prevdayrange = today- timedelta(days=6)
 start_date = prevdayrange.strftime("%Y-%m-%d")
 
+def _color_red_or_green(val):
+    color = '#9af764' if val > 0 else '#fc5a50'
+    return 'background: %s' % color
+
 def bar_with_plotly(stock):
 
     yf.pdr_override()
@@ -38,9 +42,9 @@ def bar_with_plotly(stock):
     dwl_df['slow_MA'] = dwl_df['Close'].ewm(span=slow_MA, adjust=False).mean()
     dwl_df[['fast_MA','slow_MA']] = dwl_df[['fast_MA','slow_MA']].round(2)
     dwl_df['intersectionpoint'] = np.where(dwl_df['fast_MA'] > dwl_df['slow_MA'], 1.0, 0.0)
-    dwl_df['crossover'] = dwl_df['intersectionpoint'].diff()
-    filtered_col_df = dwl_df[['Datetime','Open','High','Low','Close','crossover']].copy()
-    rslt_df = filtered_col_df[filtered_col_df['crossover'] != 0.0] 
+    dwl_df['Crossover'] = dwl_df['intersectionpoint'].diff()
+    filtered_col_df = dwl_df[['Datetime','Open','High','Low','Close','Crossover']].copy()
+    rslt_df = filtered_col_df[filtered_col_df['Crossover'] != 0.0] 
     rslt_df['Datetime'] = pd.to_datetime(rslt_df['Datetime'])
     rslt_df['Datetime'] = rslt_df['Datetime'].dt.strftime('%H:%M')
     rslt_df.insert(0,'Symbol','')
@@ -58,5 +62,10 @@ def bar_with_plotly1():
         data = bar_with_plotly(x)
         appended_data = appended_data._append(data,ignore_index=True)
     appended_data = appended_data.sort_values(by='Datetime',ascending=[False])
-    return """<meta http-equiv="refresh" content="300" />  {}.""".format(appended_data.to_html(index=False))
-    
+    #appended_data['Crossover'] = appended_data['Crossover'].round(1)
+    #styled_df = appended_data.style.set_properties(**{'background-color': 'red'})
+    styled_df = appended_data.style.applymap(_color_red_or_green, subset=['Crossover'])
+    #styled_df.style.hide_index()
+    return """<meta http-equiv="refresh" content="300" />  {}.""".format(styled_df.to_html(index=False))
+    #return styled_df.to_html(index=False)
+ 
